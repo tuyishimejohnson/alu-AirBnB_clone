fc0b4c02c5a34da3c3770bdbc8d6f1a5b5076a0d
paulinemutuku <p.mutuku@alustudent.com>
2023-09-05 13:56:59 -0700

    basemodel

---
+++ models/state.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" states module"""
+
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """ define methods for states class
+    inherit methods from Basemodel """
+
+    name = ""
